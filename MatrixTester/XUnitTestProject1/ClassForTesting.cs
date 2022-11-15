427ea0f79669bbdf094b12ae8b0c335775f89ff5
DESKTOP-NVJTH5L\Alex <alexanderbor0134@mail.ru>
2022-11-15 13:33:17 +0300

    1 итерация

---
+++ MatrixTester/XUnitTestProject1/ClassForTesting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject1
+{
+    internal class TestingClass
+    {
+        //private String[] FIO = { "Имя 1", "Имя 2", "Имя 3" };
+        public String getClientName(String FullName)
+        {
+            return FullName;
+        }
+    }
+}
---
+++ MatrixTester/XUnitTestProject1/TestClass.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace XUnitTestProject1
+{
+    [TestFixture]
+    public class Tests
+    {
+        TestingClass Class;
+        String[] FullName;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Class = new TestingClass();
+            FullName = new String[] { "Name1", "Name2", "Name3" };
+        }
+
+        [Test]
+        public void Test()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.AreEqual(FullName[i], Class.getClientName(FullName[i]));
+            }
+        }
+    }
+}
